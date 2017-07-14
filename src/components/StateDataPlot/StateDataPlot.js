52fe3f83997af3d5c0a8ecd5bca4bb678ec9e5c0
Frank LaNasa <flanasa@adharmonics.com>
2017-07-14 14:49:37 -0400

    fixed bug with circle transition

--- src/components/StateDataPlot/StateDataPlot.js
+++ src/components/StateDataPlot/StateDataPlot.js
@@ -22,29 +22,30 @@ export default function StateData(props) {
       incomeCoord,
       wellbeingCoord;
 
+  let circleClass = (hoveredState && state['state_abbrev'] != hoveredState['state_abbrev'] ? 'opaque' : '');
+
   if (props.graphType === 'scatter') {
     incomeCoord = incomePct * width + margin.left;
     wellbeingCoord = (1 - wellbeingPct) * height + margin.top;
 
     return (
       <g>
-        <circle cy={wellbeingCoord} cx={incomeCoord} r={4} />
-        <circle cy={wellbeingCoord} cx={incomeCoord} data-state={dataStateAttr} r={4} onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave}/>
+        <circle className={circleClass} cy={wellbeingCoord} cx={incomeCoord} r={4} />
+        <circle className={circleClass} cy={wellbeingCoord} cx={incomeCoord} data-state={dataStateAttr} r={4} onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave}/>
       </g>
     );
   } else {
     incomeCoord = (1 - incomePct) * height + margin.top;
     wellbeingCoord = (1 - wellbeingPct) * height + margin.top;
-    let circleClass = (hoveredState && state['state_abbrev'] != hoveredState['state_abbrev'] ? 'opaque' : ''),
-        lineClass = circleClass;
+    let lineClass = circleClass;
     lineClass += ` ${incomeMetric}-${wellbeingMetric}`;
     lineClass += ` ${(wellbeingCoord > incomeCoord ? 'green' : 'red')}`;
 
     return (
       <g>
-        <line x1={margin.left} x2={margin.left + width} y1={incomeCoord} data-state={dataStateAttr} y2={wellbeingCoord} className={lineClass} onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave}/>/>
         <circle className={circleClass} cx={margin.left} cy={incomeCoord} r={3} data-state={dataStateAttr} onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave}/>
         <circle className={circleClass} cx={margin.left + width} cy={wellbeingCoord} r={3} data-state={dataStateAttr} onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave}/>
+        <line x1={margin.left + 3} x2={margin.left + width - 3} y1={incomeCoord} data-state={dataStateAttr} y2={wellbeingCoord} className={lineClass} onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave}/>/>
       </g>
     );
   }
