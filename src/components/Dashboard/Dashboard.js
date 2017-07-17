1a81a19fdb6a4f02d7f1829d09812f9b0fcac0a5
Frank LaNasa <flanasa@adharmonics.com>
2017-07-17 12:33:37 -0400

    added dropdown indicator arrow

--- src/components/Dashboard/Dashboard.js
+++ src/components/Dashboard/Dashboard.js
@@ -10,15 +10,33 @@ export default function Dashboard(props) {
   return (
     <div className='data-selector-collection'>
       <div className={`${(appState.selectedDashButton === 'income' ? 'selected' : '')} data-selector income-metric`} data-selector='income' onClick={(appState.selectedDashButton ? handleClickOutDash : handleDashButtonSelect)} onMouseEnter={handleDashButtonSelect} onMouseLeave={handleClickOutDash} >
-        <div className='selector-btn'><p>Income <span className='medium-up-label'>Metric</span></p></div>
+        <div className='selector-btn'>
+          <p>
+            Income
+            <span className='medium-up-label'>Metric</span>
+            <span className='dropdown'>&#x25BC;</span>
+          </p>
+        </div>
         <RadioButtonCollection name='incomeMetric' options={incomeMetrics} handleChange={handleChange} {...appState}/>
       </div>
       <div className={`${(appState.selectedDashButton === 'wellbeing' ? 'selected' : '')} data-selector wellbeing-metic`} data-selector='wellbeing' onClick={(appState.selectedDashButton ? handleClickOutDash : handleDashButtonSelect)} onMouseEnter={handleDashButtonSelect} onMouseLeave={handleClickOutDash}>
-        <div className='selector-btn'><p>Wellbeing <span className='medium-up-label'>Metric</span></p></div>
+        <div className='selector-btn'>
+          <p>
+            Wellbeing
+            <span className='medium-up-label'>Metric</span>
+            <span className='dropdown'>&#x25BC;</span>
+          </p>
+        </div>
         <RadioButtonCollection name='wellbeingMetric' options={wellbeingMetrics} handleChange={handleChange} {...appState}/>
       </div>
       <div className={`${(appState.selectedDashButton === 'chart' ? 'selected' : '')} data-selector chart-type`} data-selector='chart' onClick={(appState.selectedDashButton ? handleClickOutDash : handleDashButtonSelect)} onMouseEnter={handleDashButtonSelect} onMouseLeave={handleClickOutDash}>
-        <div className='selector-btn'><p>Graph <span className='medium-up-label'>Type</span></p></div>
+        <div className='selector-btn'>
+          <p>
+            Graph
+            <span className='medium-up-label'>Type</span>
+            <span className='dropdown'>&#x25BC;</span>
+          </p>
+        </div>
         <RadioButtonCollection name='graphType' options={graphTypes} handleChange={handleChange} {...appState}/>
       </div>
     </div>
